2daf8fb55f9013d5852ab5cb5492dba6b7be3e5d
Vishnu KN <vishnunagaraj7@gmail.com>
2022-06-04 19:42:09 +0530

    Code refactored

--- src/App.js
+++ src/App.js
@@ -15,6 +15,30 @@ function App() {
     }
   }
 
+  function checkWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        gameState[a] &&
+        gameState[a] === gameState[b] &&
+        gameState[a] === gameState[c]
+      ) {
+        return gameState[a];
+      }
+    }
+    return null;
+  }
+
   function clearGame() {
     setgameState(initialGameState);
     setisUsersTurn(true);
@@ -105,37 +129,15 @@ function App() {
       return true;
     };
     if (winner) {
-      alert(`${winner} has won!`);
-      clearGame();
+      setTimeout(() => {
+        alert(`${winner} has won!`);
+        clearGame();
+      }, 50);
     } else if (noneEmpty(gameState)) {
       alert("Stalemate! Please try again");
       clearGame();
     }
   }, [gameState]);
-
-  function checkWinner() {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        gameState[a] &&
-        gameState[a] === gameState[b] &&
-        gameState[a] === gameState[c]
-      ) {
-        return gameState[a];
-      }
-    }
-    return null;
-  }
 
   return (
     <div className="app-header">
